6df57071d19ecf3a09510bb31409b3ed38da26d4
David <elflaco0800@gmail.com>
2026-05-29 08:53:58 -0400

    fix: restore Vercel adapter and hybrid mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import icon from 'astro-icon';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   site: 'https://jerg.studio',
-  output: 'hybrid',
+  output: 'static',
   adapter: vercel(),
   integrations: [tailwind(), icon()],
 });
